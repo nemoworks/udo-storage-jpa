1348215769dd2ac1e271ba77a48f737ff5b742d2
NovelistChan <774527381@qq.com>
2021-05-09 17:33:07 +0800

    add tests(with out springapp can not run)

--- src/test/java/info/nemoworks/udo/repository/StorageTests.java
+++ src/test/java/info/nemoworks/udo/repository/StorageTests.java
@@ -3,20 +3,33 @@ package info.nemoworks.udo.repository;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import info.nemoworks.udo.model.Udo;
+import info.nemoworks.udo.model.UdoSchema;
+import info.nemoworks.udo.repository.impl.H2UdoWrapperRepository;
 import info.nemoworks.udo.repository.manager.Translate;
+import info.nemoworks.udo.repository.manager.UdroSchemaManager;
+import info.nemoworks.udo.storage.UdoNotExistException;
+import info.nemoworks.udo.storage.UdoPersistException;
 import net.sf.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-//@SpringBootTest(classes = {Translate.class})
+//@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 class StorageTests {
+//
+//    @Autowired(required = true)
+//    H2UdoWrapperRepository h2UdoWrapperRepository;
 
-    public String loadFromFile() throws IOException {
-        return new String(Files.readAllBytes(Paths.get("src/test/resources/test0.json")));
+    public String loadFromFile(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 
     @Test
@@ -25,16 +38,8 @@ class StorageTests {
 
     @Test
     public void translatingTest() throws IOException {
-//        JSONObject obj = JSONObject.fromObject(this.loadFromFile());
         ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode obj = objectMapper.readTree(this.loadFromFile());
-//        obj.put("arr", "{[{val: 1}, {val: 2}]}");
-//        obj.put("obj", "{a: {b: c}}");
-//        for (Map.Entry entry: obj.entrySet()) {
-//            System.out.println(entry.getValue().getClass());
-//            System.out.println(entry.getValue() instanceof JSONArray);
-//            System.out.println(entry.getValue() instanceof JsonObject);
-//        }
+        JsonNode obj = objectMapper.readTree(this.loadFromFile("src/test/resources/test0.json"));
         Translate translate = new Translate((ObjectNode) obj);
         translate.startTrans();
         System.out.println(translate.getUTuples());
@@ -43,4 +48,19 @@ class StorageTests {
         System.out.println(translate.getobjectNode());
     }
 
+//    @Test
+//    public void udoStorageTest() throws IOException, UdoPersistException, UdoNotExistException {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JsonNode obj = objectMapper.readTree(this.loadFromFile("src/test/resources/light.json"));
+//        UdoSchema udoSchema = new UdoSchema("s-01", obj);
+//        System.out.println(h2UdoWrapperRepository.saveSchema(udoSchema));
+//        System.out.println(h2UdoWrapperRepository.findAllSchemas());
+//        System.out.println(h2UdoWrapperRepository.findSchemaById("s-01"));
+//        Udo udo = new Udo("d-01", "s-01", obj);
+//        System.out.println(h2UdoWrapperRepository.saveUdo(udo));
+//        System.out.println(h2UdoWrapperRepository.findUdoById("d-01"));
+//        System.out.println(h2UdoWrapperRepository.findUdosBySchema("s-01"));
+//        h2UdoWrapperRepository.deleteUdoById("d-01");
+//        h2UdoWrapperRepository.deleteSchemaById("s-01");
+//    }
 }
