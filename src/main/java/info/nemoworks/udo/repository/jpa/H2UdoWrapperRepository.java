fa02c26d52896dbcad5d33ce2a9dd65bb24d3cc3
NovelistChan <774527381@qq.com>
2021-06-02 19:42:08 +0800

    update findbyuri

--- src/main/java/info/nemoworks/udo/repository/jpa/H2UdoWrapperRepository.java
+++ src/main/java/info/nemoworks/udo/repository/jpa/H2UdoWrapperRepository.java
@@ -7,11 +7,10 @@ import info.nemoworks.udo.repository.jpa.entity.UdoEntity;
 import info.nemoworks.udo.storage.UdoNotExistException;
 import info.nemoworks.udo.storage.UdoPersistException;
 import info.nemoworks.udo.storage.UdoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service("WrapperService")
 public class H2UdoWrapperRepository implements UdoRepository {
@@ -25,8 +24,9 @@ public class H2UdoWrapperRepository implements UdoRepository {
     @Override
     public Udo saveUdo(Udo udo) throws UdoPersistException {
         UdoEntity entity = UdoEntity.fromUdo(udo);
-        if (udoEntityRepository.findById(entity.getId()).isPresent())
+        if (udoEntityRepository.findById(entity.getId()).isPresent()) {
             throw new UdoPersistException("Udo" + udo.getId() + "already exists.");
+        }
         udoEntityRepository.save(entity);
         return udo;
     }
@@ -34,24 +34,35 @@ public class H2UdoWrapperRepository implements UdoRepository {
     @Override
     public Udo sync(Udo udo) throws UdoPersistException {
         UdoEntity udoEntity = UdoEntity.fromUdo(udo);
-        if (udoEntityRepository.findById(udoEntity.getId()).isPresent())
+        if (udoEntityRepository.findById(udoEntity.getId()).isPresent()) {
             throw new UdoPersistException("Udo" + udo.getId() + "already exists.");
+        }
         udoEntityRepository.save(udoEntity);
         return udo;
     }
 
     @Override
     public Udo findUdoById(String id) throws UdoNotExistException {
-        if (udoEntityRepository.findById(id).isPresent())
+        if (udoEntityRepository.findById(id).isPresent()) {
             return udoEntityRepository.findById(id).get().toUdo();
-        else {
+        } else {
             throw new UdoNotExistException("Udo" + id + "does not exist.");
         }
     }
 
     @Override
+    public Udo findUdoByUri(String uri) throws UdoNotExistException {
+        if (udoEntityRepository.findByUri(uri).isPresent()) {
+            return udoEntityRepository.findByUri(uri).get().toUdo();
+        } else {
+            throw new UdoNotExistException("Udo" + uri + "does not exist.");
+        }
+    }
+
+    @Override
     public List<Udo> findUdosByType(UdoType udoType) {
-        List<UdoEntity> udoEntities = udoEntityRepository.findAllByTypeEntity(TypeEntity.from(udoType));
+        List<UdoEntity> udoEntities = udoEntityRepository
+            .findAllByTypeEntity(TypeEntity.from(udoType));
         List<Udo> udos = new ArrayList<>();
         for (UdoEntity udoEntity : udoEntities) {
             udos.add(udoEntity.toUdo());
@@ -66,8 +77,9 @@ public class H2UdoWrapperRepository implements UdoRepository {
 
     @Override
     public void deleteUdoById(String id) throws UdoNotExistException {
-        if (!udoEntityRepository.findById(id).isPresent())
+        if (!udoEntityRepository.findById(id).isPresent()) {
             throw new UdoNotExistException("Udo" + id + "does not exist.");
+        }
         udoEntityRepository.deleteById(id);
     }
 
@@ -83,24 +95,27 @@ public class H2UdoWrapperRepository implements UdoRepository {
 
     @Override
     public UdoType findTypeById(String id) throws UdoNotExistException {
-        if (!typeEntityRepository.findById(id).isPresent())
+        if (!typeEntityRepository.findById(id).isPresent()) {
             throw new UdoNotExistException("Type" + id + "does not exist.");
+        }
         return typeEntityRepository.findById(id).get().toUdoType();
     }
 
     @Override
     public UdoType saveType(UdoType udoType) throws UdoPersistException {
         TypeEntity typeEntity = TypeEntity.from(udoType);
-        if (typeEntityRepository.findById(udoType.getId()).isPresent())
+        if (typeEntityRepository.findById(udoType.getId()).isPresent()) {
             throw new UdoPersistException("Type" + udoType.getId() + "already exists.");
+        }
         typeEntityRepository.save(typeEntity);
         return udoType;
     }
 
     @Override
     public void deleteTypeById(String id) throws UdoNotExistException {
-        if (typeEntityRepository.findById(id).isPresent())
+        if (typeEntityRepository.findById(id).isPresent()) {
             throw new UdoNotExistException("Type" + id + "already exists.");
+        }
         typeEntityRepository.deleteById(id);
     }
 
--- src/main/java/info/nemoworks/udo/repository/jpa/UdoEntityRepository.java
+++ src/main/java/info/nemoworks/udo/repository/jpa/UdoEntityRepository.java
@@ -2,19 +2,21 @@ package info.nemoworks.udo.repository.jpa;
 
 import info.nemoworks.udo.repository.jpa.entity.TypeEntity;
 import info.nemoworks.udo.repository.jpa.entity.UdoEntity;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
 
 /*
 计划实现一个以Entity为实例的关系型数据库持久化层
  */
 @Repository
 public interface UdoEntityRepository extends JpaRepository<UdoEntity, String> {
+
     Optional<UdoEntity> findById(String id);
+
+    Optional<UdoEntity> findByUri(String uri);
 
     List<UdoEntity> findAllByTypeEntity(TypeEntity schemaEntity);
 
